59df89cf7e441fc3ec59cef77dff45eb176c3866
Mratyunjay Mishra <brahman.mratyunjay1311@gmail.com>
2025-12-10 17:09:42 +0530

    0

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 import Home from './Home'
 import About from './About'
 import Projects from './Projects'
-import Contact from './contact'
+import Contact from './Contact'
 
 const Layout = () => {
   return (
